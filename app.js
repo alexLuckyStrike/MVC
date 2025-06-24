1825fd09045d166f9f50f78b0204fa9a1b7b2798
Alex <alexprogressdeveloper@gmail.com>
2025-06-24 19:58:39 +0300

    adding controllers

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const errorPage = require("./routes/error");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(errorPage);
 
-app.listen(3000);
+app.listen(1000);
